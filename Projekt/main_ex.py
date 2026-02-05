8fc51375814ab4f10be9b1849f791eb3b2109b4a
Maximilian Hoh <108581748+maximilianhoh93@users.noreply.github.com>
2026-02-05 09:17:14 +0100

    Change figure save path to 'figures/'
    Updated the figure save path to a relative directory.

--- Projekt/main_ex.py
+++ Projekt/main_ex.py
@@ -3,9 +3,10 @@ from analysis_ex import Analyser
 
 # Calling up the required functions in the right order
 
-figure_save_path = "C:\\Schule\\Informatik\\Projekt\\"
+figure_save_path = "figures/"
 pipeline = ETL("data_specs.json")
 final_table = pipeline.run()
 analyser = Analyser(final_table, figure_save_path=figure_save_path)
 analyser.run()
+
 
